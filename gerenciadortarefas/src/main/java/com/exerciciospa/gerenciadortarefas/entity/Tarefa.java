ef7adee55187bcec9c21bc2cf72fb9c7a364bb57
Daniel <danieltsuyama1@gmail.com>
2026-03-23 20:46:31 -0300

    estrutura inicial + entidade Tarefa

---
+++ gerenciadortarefas/src/main/java/com/exerciciospa/gerenciadortarefas/entity/Tarefa.java
@@ -0,0 +1,47 @@
+package com.exerciciospa.gerenciadortarefas.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_tarefa")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_tarefa")
+    private String nome;
+
+    private String descricao;
+
+    private Boolean concluida;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Boolean getConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(Boolean concluida) {
+        this.concluida = concluida;
+    }
+}
---
+++ gerenciadortarefas/src/test/java/com/exerciciospa/gerenciadortarefas/GerenciadortarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exerciciospa.gerenciadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadortarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
